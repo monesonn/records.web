01f9476197432b4eaf7fc3eae68d9cf1199783c2
monesonn <monesonn@tuta.io>
2021-05-03 14:37:16 +0300

    split and minimize main file

--- app.go
+++ app.go
@@ -1,48 +1,24 @@
 package main
 
 import (
-	"context"
-	"fmt"
 	"log"
-	"os"
 
 	"github.com/monesonn/records.web/pkg/configs"
+	"github.com/monesonn/records.web/pkg/routes"
+	_ "github.com/monesonn/records.web/platform/database"
 
 	"github.com/gofiber/fiber/v2"
-	"github.com/jackc/pgx/v4"
-	"github.com/joho/godotenv"
+
+	_ "github.com/joho/godotenv/autoload"
 )
 
 func main() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Fatal("Error loading .env file")
-	}
-
-	conn, err := pgx.Connect(context.Background(), os.Getenv("DATABASE_URL"))
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-		os.Exit(1)
-	}
-	defer conn.Close(context.Background())
-
 	config := configs.FiberConfig()
-
-	// Create new Fiber instance
 	app := fiber.New(config)
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return c.SendString("I made a ☕ for you!")
-	})
+	routes.PublicRoutes(app)
+	routes.NotFoundRoute(app)
+	routes.SwaggerRoute(app)
 
-	app.Use(func(c *fiber.Ctx) error {
-		return c.SendStatus(404) // => 404 "Not Found"
-	})
-
-	// Start server on http://localhost:3000
 	log.Fatal(app.Listen(":3000"))
-}
-
-func hello(c *fiber.Ctx) error {
-	return c.SendString("Oopsie!")
 }
