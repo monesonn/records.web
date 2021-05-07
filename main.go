f2b5b3eb3e6f647ac762b582892c80cba1f2c249
monesonn <monesonn@tuta.io>
2021-05-07 15:05:30 +0300

    add swagger tags & middlewares

--- main.go
+++ main.go
@@ -4,17 +4,27 @@ import (
 	"log"
 
 	"github.com/monesonn/records.web/pkg/configs"
+	"github.com/monesonn/records.web/pkg/middleware"
 	"github.com/monesonn/records.web/pkg/routes"
 	_ "github.com/monesonn/records.web/platform/database"
 
 	"github.com/gofiber/fiber/v2"
 
+	_ "github.com/monesonn/records.web/docs" // load API Docs files (Swagger)
+
 	_ "github.com/joho/godotenv/autoload"
 )
 
+// @title API
+// @version 1.0
+// @description This is an auto-generated API Docs.
+// @termsOfService http://swagger.io/terms/
+// @BasePath /api
 func main() {
 	config := configs.FiberConfig()
 	app := fiber.New(config)
+
+	middleware.FiberMiddleware(app)
 
 	routes.PublicRoutes(app)
 	routes.NotFoundRoute(app)
