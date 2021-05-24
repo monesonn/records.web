30fe907afdc7bc937761fd2540bd79d0e4d2e7fd
monesonn <monesonn@tuta.io>
2021-05-24 22:33:33 +0300

    update public routes

--- pkg/routes/public_routes.go
+++ pkg/routes/public_routes.go
@@ -10,19 +10,15 @@ func PublicRoutes(a *fiber.App) {
 	route := a.Group("api")
 
 	a.Static("/", "./public")
+
 	// Routes for GET method:
-	// route.Get("/", func(c *fiber.Ctx) error {
-	// a.Static("/", "./public")
-	// })
-	route.Get("/genre", controllers.GetGenres)
+	route.Get("/genres", controllers.GetGenres)
 	route.Get("/genre/:id", controllers.GetGenre)
-	route.Get("/artist", controllers.GetArtists)
+	route.Get("/artists", controllers.GetArtists)
 	route.Get("/artist/:id", controllers.GetArtist)
-	route.Get("/record", controllers.GetRecords)
+	route.Get("/record", controllers.GetRecordURL)
+	route.Get("/records", controllers.GetRecords)
 	route.Get("/record/:id", controllers.GetRecord)
-	// route.Get("/record/artist/:artist", controllers.GetRecordArgs)
-	route.Get("/record/artist/:artist", controllers.GetRecordArgs)
-	route.Get("/record/artist/:artist/genre/:genre", controllers.GetRecordArgs)
 	// Routes for POST method:
 	route.Post("/user/sign/up", controllers.UserSignUp) // register a new user
 	route.Post("/user/sign/in", controllers.UserSignIn) // auth
